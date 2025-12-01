bf1257dfdded272faf002cca90717c8d548dcdf8
Olivia Thetgyi <olivia.thetgyi@news.co.uk>
2025-12-01 23:23:20 +0000

    Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+        <label>
+            Add a word to the dictionary:
+            <input type="text" />
+        </label>
+    </div>
+  );
+}
