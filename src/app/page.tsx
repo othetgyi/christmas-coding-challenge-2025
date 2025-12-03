ab81792b804a221a97d62fcd03ea70690ac00056
Olivia Thetgyi <olivia.thetgyi@news.co.uk>
2025-12-03 21:14:01 +0000

    Add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-export default function Home() {
-  return (
-    <div>
-        <label>
-            Add a word to the dictionary:
-            <input type="text" />
-        </label>
-    </div>
-  );
+"use client"
+const Home = () => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log("***formJson**", formJson);
+    }
+
+    return (
+        <form method={"POST"} onSubmit={handleSubmit}>
+            <label>
+                Add a word to the dictionary: <input type="text" name="word"/>
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
+
+export default Home;
